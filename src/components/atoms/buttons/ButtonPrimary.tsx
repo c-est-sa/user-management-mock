307870a74490625bcecb975215297a047c29f5da
Sacha <150120887+c-est-sa@users.noreply.github.com>
2024-07-29 00:56:44 -0700

    chore: subdivide module into component

---
+++ src/components/atoms/buttons/ButtonPrimary.tsx
@@ -0,0 +1,18 @@
+import React, { FC, ReactNode, memo } from "react";
+import { Button } from "@chakra-ui/react";
+
+type Props = {
+  children: ReactNode;
+};
+
+const ButtonPrimary: FC<Props> = memo((props) => {
+  const { children } = props;
+
+  return (
+    <Button bg="teal.400" color="white" _hover={{ opacity: 0.8 }}>
+      {children}
+    </Button>
+  );
+});
+
+export default ButtonPrimary;
--- src/components/pages/Login.tsx
+++ src/components/pages/Login.tsx
@@ -1,13 +1,7 @@
-import {
-  Box,
-  Button,
-  Divider,
-  Flex,
-  Heading,
-  Input,
-  Stack,
-} from "@chakra-ui/react";
+import { Box, Divider, Flex, Heading, Input, Stack } from "@chakra-ui/react";
 import React, { FC, memo } from "react";
+
+import ButtonPrimary from "../atoms/buttons/ButtonPrimary";
 
 const Login: FC = memo(() => {
   return (
@@ -16,12 +10,12 @@ const Login: FC = memo(() => {
         <Heading as="h1" size="lg" textAlign="center">
           User Management App
         </Heading>
+
         <Divider my={4}></Divider>
+
         <Stack spacing={6} py={4} px={10}>
           <Input placeholder="user ID"></Input>
-          <Button bg="teal.400" color="white" _hover={{ opacity: 0.8 }}>
-            Log in
-          </Button>
+          <ButtonPrimary>Log in</ButtonPrimary>
         </Stack>
       </Box>
     </Flex>
